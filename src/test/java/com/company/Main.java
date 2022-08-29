0c68606e32e2bba0cae0d7aa6a7c71fb5c8b8852
github_yavuz.selim.ceylan <yavuz.selim.ceylan@gmail.com>
2022-08-29 19:04:51 -0400

    solar system ready to be deployed

--- src/test/java/com/company/Main.java
+++ src/test/java/com/company/Main.java
@@ -2,8 +2,25 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        SolarSystem inception =new SolarSystem();
+        inception.sun=1;
+        inception.moon=4;
+        inception.planets=20;
 
+        System.out.println("the basic structure is ready");
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
